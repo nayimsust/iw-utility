6222d4c7a1212dc8537e0c51b370f6e777f6ea03
nazmul@eyeball.com <nazmul@eyeball.com>
2016-11-23 19:41:31 -0800

    just make a new c++ hello world project using clang

---
+++ cmake-build-debug/CMakeFiles/iw_utility.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iw_utility.dir/main.cpp.o"
+  "iw_utility.pdb"
+  "iw_utility"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iw_utility.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
